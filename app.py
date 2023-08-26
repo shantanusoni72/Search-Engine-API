05568b14b751c64bc5e63157e03b1c4a8ca2e011
Shantanu <75296055+shantanusoni72@users.noreply.github.com>
2023-08-26 01:25:06 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ def search():
 
     service = Service()
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--verbose')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(service=service, options=options)
 
     results = {}
